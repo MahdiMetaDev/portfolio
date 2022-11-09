6ae2617453124aa9c8af85f9bff6c0e02afad189
mahdi <mmf1370py@gmail.com>
2022-11-08 21:28:24 -0800

    start

---
+++ accounts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'is_staff']
+    
